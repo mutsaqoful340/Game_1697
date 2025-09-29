c76fe117a186bd6bf026b73cb877f38bd720950c
Fikri Ahmad <rukimevick@gmail.com>
2025-09-29 16:01:25 +0700

    UI_Selector.cs

---
+++ Assets/Scripts/UI/UI_Selector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Selector : MonoBehaviour
+{
+    public void OnClick_ButtonPlay()
+    {
+        Debug.Log("Play Button clicled");
+    }
+}
