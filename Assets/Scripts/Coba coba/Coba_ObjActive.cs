bc9d6488e07a8a487589a84c87db1553c2da5ea6
Fikri Ahmad <rukimevick@gmail.com>
2025-09-30 14:46:39 +0700

    a

--- Assets/Scripts/Coba coba/Coba_ObjActive.cs
+++ Assets/Scripts/Coba coba/Coba_ObjActive.cs
@@ -13,9 +13,33 @@ public class Coba_ObjActive : MonoBehaviour
         obj.SetActive(false);
     }
 
-    private void Update()
+    void Update()
     {
+        Vector3[] screenPoints = {
+        new Vector3(0, 0, 0), // Bottom-left
+        new Vector3(Screen.width, 0, 0), // Bottom-right
+        new Vector3(0, Screen.height, 0), // Top-left
+        new Vector3(Screen.width, Screen.height, 0) // Top-right
+    };
+
+        foreach (Vector3 screenPoint in screenPoints)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Check if the hit object has the desired tag
+                if (hit.collider.CompareTag("ObjectA"))
+                {
+                    Debug.Log("Detected ObjectA object at screen corner.");
+                }
+            }
+        }
     }
+
+
+
 
     private void SetGameObjActive()
     {
