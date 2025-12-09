e089ce39af3289665cf442f317b630f35c402d5c
Fikri Ahmad <rukimevick@gmail.com>
2025-12-09 14:56:31 +0700

    Adding Stage 2

--- Assets/Scripts/Stage_1Active.cs
+++ Assets/Scripts/Stage_1Active.cs
@@ -1,27 +1,28 @@
-using System;
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stage_1Active : MonoBehaviour
+public class StageActive : MonoBehaviour
 {
     public enum StageType
     {
         FindSword,
-        TakeSword,
-        Destroy
+        RuneStone,
+        FeedSword,
+        TakeSowrd,
+        Destroy,
     }
 
-
-    public StageType state;
+    public StageType State;
     public List<Quest> CheckData;
-    public Transform player;
+    public Transform Player;
     public int Index;
 
     [Serializable]
     public class Quest
     {
-        public string name;
+        public string Name;
         public int Qty;
     }
 
@@ -29,14 +30,17 @@ public class Stage_1Active : MonoBehaviour
     {
         get
         {
-            return state;
+            return State;
         }
         set
         {
-            switch (state)
+            switch (State)
             {
                 case StageType.FindSword:
                     Act1_FindSword(true);
+                    break;
+                case StageType.RuneStone:
+                    Act2_RuneStone(true);
                     break;
             }
         }
@@ -44,36 +48,102 @@ public class Stage_1Active : MonoBehaviour
 
     private void Act1_FindSword(bool setup)
     {
-        if (setup) // Preparation
+        if (setup) //prepare
         {
             CheckData = new List<Quest>()
             {
-                new()
-                {
-                    name = "Talk to the NPC.", Qty = 0
-                },
-                new()
-                {
-                    name = "Find the Legendary Sword.", Qty = 0
-                }
+                new() { Name = "Talk to the NPC.", Qty = 0 },
+                new() { Name = "Find the Legendary Sword.", Qty = 0},
             };
             var obj = transform.Find("Act_1").GetChild(0);
             obj.gameObject.SetActive(true);
             obj.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = InteractionAct1_Handler;
-            Debug.Log(CheckData[0].name);
+            Debug.Log(CheckData[0].Name);
             Index = 0;
         }
-        else // Monitoring
+        else //monitoring
         {
+            if (Index >= CheckData.Count)
+            {
+                var obj = transform.Find("Act_1").GetChild(0);
+                obj.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = null;
+                Destroy(obj.parent.gameObject);
+                StageRoute = StageType.RuneStone;
+                return;
+            }
             var target = transform.Find("Act_1").GetChild(0);
-            CheckData[Index].Qty = (int)Vector3.Distance(player.position, target.position);
-            Debug.Log(CheckData[0].name + " berjarak" + CheckData[0].Qty);
+            CheckData[Index].Qty = (int)Vector3.Distance(Player.position, target.position);
+            Debug.Log(CheckData[0].Name + " berjarak " + CheckData[0].Qty);
+        }
+    }
+    private void InteractionAct1_Handler()
+    {
+        if (CheckData[Index].Qty <= 1)
+        {
+            if (++Index < CheckData.Count)
+            {
+                var point = transform.Find("Act_1").GetChild(1);
+                point.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = InteractionAct1_Handler;
+                point.gameObject.SetActive(true);
+                transform.Find("Act_1").GetChild(0).SetAsLastSibling();
+            }
         }
     }
 
-    private void InteractionAct1_Handler()
+    private void Act2_RuneStone(bool setup)
     {
+        if (setup) //prepare
+        {
+            CheckData = new List<Quest>()
+            {
+                new() { Name = "Talk to NPC1.", Qty = 0},
+                new() { Name = "Talk to NPC2.", Qty = 0},
+                new() { Name = "Talk to NPC3.", Qty = 0},
+            };
+            var obj = transform.Find("Act_2").GetChild(0);
+            obj.gameObject.SetActive(true);
+            obj.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = InteractionAct2_Handler;
+            Debug.Log(CheckData[0].Name);
+            Index = 2;
+        }
+        else //monitoring
+        {
+            if (Index >= CheckData.Count)
+            {
+                var obj = transform.Find("Act_2").GetChild(0);
+                obj.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = null;
+                Destroy(obj.parent.gameObject);
+                StageRoute = StageType.FeedSword;
+                return;
+            }
+            var target = transform.Find("Act_2").GetChild(0);
+            CheckData[Index].Qty = (int)Vector3.Distance(Player.position, target.position);
+            Debug.Log(CheckData[0].Name + " berjarak " + CheckData[0].Qty);
+        }
+    }
 
+    private void InteractionAct2_Handler()
+    {
+        if (CheckData[Index].Qty <= 3)
+        {
+            if (++Index < CheckData.Count)
+            {
+                var point = transform.Find("Act_2").GetChild(1);
+                point.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = InteractionAct1_Handler;
+                point.gameObject.SetActive(true);
+                transform.Find("Act_2").GetChild(0).SetAsLastSibling();
+            }
+        }
+        if (CheckData[Index].Qty <= 4)
+        {
+            if (++Index < CheckData.Count)
+            {
+                var point = transform.Find("Act_2").GetChild(2);
+                point.GetChild(0).GetComponent<DialogActive>().OnFinishedAction = InteractionAct1_Handler;
+                point.gameObject.SetActive(true);
+                transform.Find("Act_2").GetChild(0).SetAsLastSibling();
+            }
+        }
     }
 
     private void Start()
@@ -83,10 +153,14 @@ public class Stage_1Active : MonoBehaviour
 
     private void Update()
     {
-        switch (state)
+        Debug.Log(CheckData.Count);
+        switch (State)
         {
             case StageType.FindSword:
                 Act1_FindSword(false);
+                break;
+            case StageType.RuneStone:
+                Act2_RuneStone(false);
                 break;
         }
     }
